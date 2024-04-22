96734e254e5d9b97f23eda11b9020d8ecf4077d9
Jeniba29 <135032806+Jeniba29@users.noreply.github.com>
2024-04-22 18:13:03 +0000

    1

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask,render_template,request
+import pickle
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+@app.route('/prediction',methods=['GET','POST'])
+def predict():
+    if request.method == 'POST':
+        gender = int(request.form['gender'])
+        age = int(request.form['age'])
+        estimatedsalary =int(request.form['estimatedsalary'])
+        
+        model = pickle.load(open('model.pkl','rb'))
+        prediction= model.predict([[(gender,age,estimatedsalary)]])
+        print(prediction[0])
+        
+        return render_template('prediction.html',target=prediction[0])
+    
+if __name__ =='__main__':
+
+   app.run(port=5001,debug=True)
