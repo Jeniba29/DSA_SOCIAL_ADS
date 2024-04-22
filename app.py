ce1e395816cae6f422fa9f78b2442e6c5c399501
Jeniba29 <135032806+Jeniba29@users.noreply.github.com>
2024-04-22 19:40:03 +0000

    2

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template,request
 import pickle
+
 app = Flask(__name__)
 
 
@@ -13,13 +14,14 @@ def predict():
         gender = int(request.form['gender'])
         age = int(request.form['age'])
         estimatedsalary =int(request.form['estimatedsalary'])
-        
+
         model = pickle.load(open('model.pkl','rb'))
+
         prediction= model.predict([[(gender,age,estimatedsalary)]])
         print(prediction[0])
-        
-        return render_template('prediction.html',target=prediction[0])
+
+    return render_template('prediction.html',target=prediction[0])
     
 if __name__ =='__main__':
 
-   app.run(port=5001,debug=True)
+   app.run(port=5002,debug=True)
