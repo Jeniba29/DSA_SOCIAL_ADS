2f85e48a786274f199ece581382565fa7e52dbd2
Jeniba29 <135032806+Jeniba29@users.noreply.github.com>
2024-04-23 05:40:50 +0000

    5

--- app.py
+++ app.py
@@ -11,13 +11,12 @@ def hello():
 @app.route('/prediction',methods=['GET','POST'])
 def predict():
     if request.method == 'POST':
-        gender = int(request.form['gender'])
         age = int(request.form['age'])
         estimatedsalary =int(request.form['estimatedsalary'])
 
         model = pickle.load(open('model.pkl','rb'))
 
-        prediction= model.predict([[(gender,age,estimatedsalary)]])
+        prediction= model.predict([(age,estimatedsalary)])
         print(prediction[0])
 
     return render_template('prediction.html',target=prediction[0])
